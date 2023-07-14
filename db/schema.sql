254ba017951ed24ae43d5dc098b3785069c822d5
Nick <chargerfan428@aol.com>
2023-07-13 19:33:23 -0700

    edited db to hold usernames and passwords

--- db/schema.sql
+++ db/schema.sql
@@ -1,3 +1,10 @@
-DROP DATABASE IF EXISTS tech_blog_db;
+DROP DATABASE IF EXISTS stocktalk;
 
-CREATE DATABASE tech_blog_db;
+CREATE DATABASE stocktalk;
+
+USE stocktalk;
+CREATE TABLE users (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  username VARCHAR(50) NOT NULL UNIQUE,
+  password VARCHAR(255) NOT NULL
+);
